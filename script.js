75eebb8c05e49fc9285fbc3c659266f000bc137e
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-11-05 01:44:08 -0500

    player and movement added

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 import kaplay from "https://unpkg.com/kaplay@3001.0.0-alpha.20/dist/kaplay.mjs";
 
 kaplay()
+
+
+loadSprite("player","/assets/bean.png");
+
+
+let SPEED = 500;
+
+const player = add([
+    sprite("player"),
+    pos(center()),
+    "player"
+]);
+
+onKeyDown("a", () => {
+
+    player.move(-SPEED, 0);
+});
+
+onKeyDown("d", () => {
+    player.move(SPEED, 0);
+});
+
+onKeyDown("w", () => {
+    player.move(0, -SPEED);
+});
+
+onKeyDown("s", () => {
+    player.move(0, SPEED);
+});
